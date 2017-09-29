a2eb8a644003b713911847e5bc9f6abac4afb447
Bob Lloyd <blloyd@blackducksoftware.com>
2017-09-29 15:08:35 -0400

    Automatic: Updates from the build for release 4.1.4

--- docker-swarm/bin/system_check.sh
+++ docker-swarm/bin/system_check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-HUB_VERSION=${HUB_VERSION:-4.1.3}
+HUB_VERSION=${HUB_VERSION:-4.1.4}
 OUTPUT_FILE=${SYSTEM_CHECK_OUTPUT_FILE:-"system_check.txt"}
 CPUS_REQUIRED=4
 # Our RAM requirements are as follows:
